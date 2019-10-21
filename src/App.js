7a48a3cd562f28866e0a7886ab6e78bebb87f7f1
Adeola Adeleke <apps@simplyswift.com>
2019-10-21 18:36:54 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import google_logo from './img/gwbg.png';
+import andela_logo from './img/andela_rect.png';
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="sponsor">
+          <div id="column1">
+              <img src={google_logo} className="App-logo" alt="Google logo" />
+              <p>Google</p>
+          </div>
+          <div id="column2">
+              <img src={andela_logo} className="App-logo" alt="Andela logo" />
+              <p>Andela</p>
+          </div>
+        </div>
+
+        <p>Thank you guys for the opportunity to learn about Google Cloud Engine.</p>
+        <p> The training has exposed to how the cloud works and how to deploy system using the Google Cloud. </p>
+        <p> Designed by&nbsp;&nbsp;
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://linkedin.com/in/lekeadeola"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Adeola Adeleke
         </a>
+        </p>
       </header>
     </div>
   );
